ad4e7594585d12da340e24d21e5ba4e2c397d573
Pavel Komarov <pkomarov53@gmail.com>
2024-03-06 11:11:15 +0300

    Обновление скриптинга

--- static/js/index_scripts.js
+++ static/js/index_scripts.js
@@ -11,6 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	let companyButton = document.getElementById('tapeButton');
+	let companySection = document.getElementById('tape_type_1_section');
+	companyButton.addEventListener('click', function(event) {
+		event.preventDefault();
+		let yOffset = companySection.getBoundingClientRect().top - 150;
+		window.scrollBy({
+			top: yOffset,
+			left: 0,
+			behavior: 'smooth'
+		});
+	});
+});
+
 document.addEventListener('DOMContentLoaded', function() {
 	let companyButton = document.getElementById('contactsButton');
 	let companySection = document.getElementById('contacts-section');
@@ -21,12 +36,13 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	});
 });
+
 document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
+	let companyButton = document.getElementById('tape_type_1');
+	let companySection = document.getElementById('tape_type_1_section');
 	companyButton.addEventListener('click', function(event) {
 		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
+		let yOffset = companySection.getBoundingClientRect().top - 100;
 		window.scrollBy({
 			top: yOffset,
 			left: 0,
@@ -34,12 +50,13 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	});
 });
+
 document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
+	let companyButton = document.getElementById('tape_type_2');
+	let companySection = document.getElementById('tape_type_2_section');
 	companyButton.addEventListener('click', function(event) {
 		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
+		let yOffset = companySection.getBoundingClientRect().top - 100;
 		window.scrollBy({
 			top: yOffset,
 			left: 0,
@@ -47,91 +64,43 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	});
 });
+
 document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
-	companyButton.addEventListener('click', function(event) {
-		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
-		window.scrollBy({
-			top: yOffset,
-			left: 0,
-			behavior: 'smooth'
-		});
-	});
+    let companyButton = document.getElementById('designsButton');
+    let companySection = document.getElementById('designs-section');
+    companyButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        let yOffset = companySection.getBoundingClientRect().top - 215;
+        window.scrollBy({
+            top: yOffset,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    $(".img-fluid").click(function(event) {
+        event.stopPropagation();
+
+        let $clickedImage = $(this);
+        if (!$clickedImage.hasClass("zoomed")) {
+            $(".img-fluid").removeClass("zoomed");
+            $clickedImage.addClass("zoomed");
+            // Вызов функции пролистывания при увеличении картинки
+            let yOffset = companySection.getBoundingClientRect().top - 215;
+            window.scrollBy({
+                top: yOffset,
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            $clickedImage.removeClass("zoomed");
+        }
+    });
+
+    $(document).click(function(event) {
+        if (!$(event.target).closest('.img-fluid').length) {
+            $(".img-fluid").removeClass("zoomed");
+        }
+    });
 });
-document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
-	companyButton.addEventListener('click', function(event) {
-		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
-		window.scrollBy({
-			top: yOffset,
-			left: 0,
-			behavior: 'smooth'
-		});
-	});
-});
-document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
-	companyButton.addEventListener('click', function(event) {
-		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
-		window.scrollBy({
-			top: yOffset,
-			left: 0,
-			behavior: 'smooth'
-		});
-	});
-});
-document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
-	companyButton.addEventListener('click', function(event) {
-		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
-		window.scrollBy({
-			top: yOffset,
-			left: 0,
-			behavior: 'smooth'
-		});
-	});
-});
-document.addEventListener('DOMContentLoaded', function() {
-	let companyButton = document.getElementById('designsButton');
-	let companySection = document.getElementById('designs-section');
-	companyButton.addEventListener('click', function(event) {
-		event.preventDefault();
-		let yOffset = companySection.getBoundingClientRect().top - 215;
-		window.scrollBy({
-			top: yOffset,
-			left: 0,
-			behavior: 'smooth'
-		});
-	});
-});
-$(".img-fluid").click(function(event) {
-	event.stopPropagation();
-	let $clickedImage = $(this);
-	if(!$clickedImage.hasClass("zoomed")) {
-		$(".img-fluid").removeClass("zoomed");
-		$clickedImage.addClass("zoomed");
-		// Вызов функции пролистывания при увеличении картинки
-		let yOffset = companySection.getBoundingClientRect().top - 215;
-		window.scrollBy({
-			top: yOffset,
-			left: 0,
-			behavior: 'smooth'
-		});
-	} else {
-		$clickedImage.removeClass("zoomed");
-	}
-});
-$(document).click(function(event) {
-if(!$(event.target).closest('.img-fluid').length) {
-	$(".img-fluid").removeClass("zoomed");
-}
-});
-});
+
