82d61a28196b543a3c3a25c5636c0c1a4e7f821f
Pavel Komarov <pkomarov53@gmail.com>
2024-03-08 16:06:53 +0300

    Обновление

--- static/js/index_scripts.js
+++ static/js/index_scripts.js
@@ -24,6 +24,19 @@ document.addEventListener("DOMContentLoaded", function () {
     setInterval(nextParagraph, 5000); // Интервал в миллисекундах (в данном случае 3 секунды)
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    let companyButton = document.getElementById('companyButton');
+    let companySection = document.getElementById('company-section');
+    companyButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        let yOffset = companySection.getBoundingClientRect().top - 150;
+        window.scrollBy({
+            top: yOffset,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     let companyButton = document.getElementById('tapeButton');
@@ -36,6 +49,42 @@ document.addEventListener('DOMContentLoaded', function() {
             left: 0,
             behavior: 'smooth'
         });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let companyButton = document.getElementById('designsButton');
+    let companySection = document.getElementById('designs-section');
+    companyButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        let yOffset = companySection.getBoundingClientRect().top - 215;
+        window.scrollBy({
+            top: yOffset,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+    $(".img-fluid").click(function(event) {
+        event.stopPropagation();
+        let $clickedImage = $(this);
+        if(!$clickedImage.hasClass("zoomed")) {
+            $(".img-fluid").removeClass("zoomed");
+            $clickedImage.addClass("zoomed");
+            // Вызов функции пролистывания при увеличении картинки
+            let yOffset = companySection.getBoundingClientRect().top - 215;
+            window.scrollBy({
+                top: yOffset,
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            $clickedImage.removeClass("zoomed");
+        }
+    });
+    $(document).click(function(event) {
+        if(!$(event.target).closest('.img-fluid').length) {
+            $(".img-fluid").removeClass("zoomed");
+        }
     });
 });
 
@@ -145,41 +194,5 @@ document.addEventListener('DOMContentLoaded', function() {
             left: 0,
             behavior: 'smooth'
         });
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    let companyButton = document.getElementById('designsButton');
-    let companySection = document.getElementById('designs-section');
-    companyButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        let yOffset = companySection.getBoundingClientRect().top - 215;
-        window.scrollBy({
-            top: yOffset,
-            left: 0,
-            behavior: 'smooth'
-        });
-    });
-    $(".img-fluid").click(function(event) {
-        event.stopPropagation();
-        let $clickedImage = $(this);
-        if(!$clickedImage.hasClass("zoomed")) {
-            $(".img-fluid").removeClass("zoomed");
-            $clickedImage.addClass("zoomed");
-            // Вызов функции пролистывания при увеличении картинки
-            let yOffset = companySection.getBoundingClientRect().top - 215;
-            window.scrollBy({
-                top: yOffset,
-                left: 0,
-                behavior: 'smooth'
-            });
-        } else {
-            $clickedImage.removeClass("zoomed");
-        }
-    });
-    $(document).click(function(event) {
-        if(!$(event.target).closest('.img-fluid').length) {
-            $(".img-fluid").removeClass("zoomed");
-        }
     });
 });
