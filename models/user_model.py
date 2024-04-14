a7a8c218bd9e90ddacd65c806a997cd9c9947f18
Pavel Komarov <pkomarov53@gmail.com>
2024-04-14 17:09:02 +0300

    Updates

--- models/user_model.py
+++ models/user_model.py
@@ -1,18 +1,27 @@
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
+db = SQLAlchemy()
 
-class User(UserMixin):
-    def __init__(self, username, password):
-        self.id = id
-        self.username = username
-        self.password = generate_password_hash(password)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    def is_active(self):
-        return True
+        return check_password_hash(self.password_hash, password)
 
     def get_id(self):
-        return self.username
+        return str(self.id)
+
+    @staticmethod
+    def is_active():
+        return True
+
+    @staticmethod
+    def is_authenticated():
+        return True
